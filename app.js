c73f40dd722c992adf0b3133f32726c1ac5b631b
Akivs <akivmoovex@gmail.com>
2024-04-26 02:42:10 +0300

    Trying DB connection

--- app.js
+++ app.js
@@ -22,12 +22,6 @@ const client = (() => {
       });
   } })();
 
-
-/*const client = new Client({
-  connectionString: process.env.DATABASE_URL || 'postgresql://postgres://u724pr3kon35lj:pdc7baf32a7fb356338b919f91995eef15476787ff020e1ebde3f01945845357b@c2dr1dq7r4d57i.cluster-czz5s0kz4scl.eu-west-1.rds.amazonaws.com:5432/dahf5q0eco5cc7',
-  ssl: process.env.DATABASE_URL ? true : false
-})*/
-
 client.connect();
 
 client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
@@ -38,9 +32,6 @@ client.query('SELECT table_schema,table_name FROM information_schema.tables;', (
   client.end();
 });
 //End DB connection code
-
-
-
 
 ///Sequelize
 const { sequelize } = require('./models')
@@ -65,8 +56,7 @@ app.post('/url', function(req, res) {
     res.sendFile(path.join(__dirname + '/html2.html'));
 });
 
-//app.listen(port, () => console.log(`url-shortener listening on port ${port}!`));
 
 app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode %s", this.address().port, app.settings.env);
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
